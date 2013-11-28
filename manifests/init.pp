2828c122547097899a6c691d4eb41aa7f039cda6
Adam Ochonicki <adam@fromonesrc.com>
2013-11-28 12:59:06 -0500

    Please the linter

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,7 @@
 class onepassword {
   $host = 'd13itkw33a7sus.cloudfront.net'
   $path = 'dist/1P/mac4/1Password-4.0.9.zip'
-   
+
   package { '1Password':
     source   => "https://${host}/${path}",
     provider => 'compressed_app'
