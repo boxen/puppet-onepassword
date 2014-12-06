70dd800631a48aba4829a504d39a1283b10c8f99
Mark Tareshawty <tarebyte@gmail.com>
2014-12-06 12:35:48 -0500

    update to 5.0.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 #     include onepassword
 class onepassword {
   $host = 'd13itkw33a7sus.cloudfront.net'
-  $path = 'dist/1P/mac4/1Password-5.0.1.zip'
+  $path = 'dist/1P/mac4/1Password-5.0.2.zip'
 
   package { '1Password 5':
     source   => "https://${host}/${path}",
